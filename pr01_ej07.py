4c492f4f4e20e93877f17f338c306daa0beb63f3
IanUJI01 <al418368@uji.es>
2021-09-30 16:50:32 +0200

    Update pr01_ej07.py

--- pr01_ej07.py
+++ pr01_ej07.py
@@ -1,24 +1,24 @@
 from turtle import *
-pantalla = Screen()
-pantalla.setup(425,225)
-pantalla.screensize(400,200)
+pan = Screen()
+pan.setup(425,225)
+pan.screensize(400,200)
 #datos y calculos
 tortuga = Turtle()
-suspensos = int(input('Dime el numero de suspensos: '))
+sus = int(input('Dime el numAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAe sus: '))
 aprobados = int(input('Dime el numero de aprobados: '))
 notables = int(input('Dime el numero de notables: '))
 sobresalientes = int(input('Dime el numero de sobresalientes: '))
-total = suspensos+aprobados+notables+sobresalientes
-p_suspensos = (suspensos/total)*100
+total = sus+aprobados+notables+sobresalientes
+p_sus = (sus/total)*100
 p_aprobados = (aprobados/total)*100
 p_notables = (notables/total)*100
 p_sobresalientes = (sobresalientes/total)*100
 #angulos
-angulo_sus = p_suspensos*360/100
+angulo_sus = p_sus*360/100
 angulo_apr = p_aprobados*360/100
 angulo_not = p_notables*360/100
 angulo_sob = p_sobresalientes*360/100
-#suspensos
+#sus
 tortuga.penup()
 tortuga.goto(0,0)
 tortuga.pencolor('red')
@@ -31,7 +31,7 @@ tortuga.goto(0,0)
 
 tortuga.right(90+angulo_sus/2)
 tortuga.forward(150/2)
-tortuga.write('suspensos')
+tortuga.write('sus')
 tortuga.backward(150/2)
 tortuga.left(90+angulo_sus/2)
 tortuga.right(90)
